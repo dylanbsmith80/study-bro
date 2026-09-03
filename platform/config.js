ef5489fda5ec98e87daf4e4aca368dd9b5fc68e6
Dylan Smith <dylansmith@Dylans-MacBook-Air-2.local>
2026-09-03 16:24:03 -0700

    Connect platform to Supabase and sync account edits

--- platform/config.js
+++ platform/config.js
@@ -1,5 +1,5 @@
 window.STUDY_BRO_CONFIG = {
-  supabaseUrl: "__SUPABASE_URL__",
-  supabasePublishableKey: "__SUPABASE_PUBLISHABLE_KEY__",
+  supabaseUrl: "https://qwzayhxclcprhgthbnlx.supabase.co",
+  supabasePublishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3emF5aHhjbGNwcmhndGhibmx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0NzA1MDAsImV4cCI6MjEwNDA0NjUwMH0.vESjXtniTg5swMpHydHyzSnVRzTxf4mBvYSNBI1UrdE",
   homeUrl: "__HOME_URL__"
 };
